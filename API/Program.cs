83c22ac0223a866d00f311353d3692116830576e
Andy Allison <andy@talkbe.com>
2019-09-21 17:40:21 +0100

    Adding Axios

--- API/Program.cs
+++ API/Program.cs
@@ -13,26 +13,30 @@ using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+      var host = CreateWebHostBuilder(args).Build();
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        try
         {
-          var host = CreateWebHostBuilder(args).Build();
-          using (var scope = host.Services.CreateScope()){
-              var services = scope.ServiceProvider;
-              try {
-                  var context = services.GetRequiredService<DataContext>();
-                  context.Database.Migrate();
-              } catch(Exception ex){
-                  var logger = services.GetRequiredService<ILogger>();
-                  logger.LogError(ex, "An error occured during migration");
-              }
-          }
-          host.Run();
+          var context = services.GetRequiredService<DataContext>();
+          context.Database.Migrate();
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        catch (Exception ex)
+        {
+          var logger = services.GetRequiredService<ILogger>();
+          logger.LogError(ex, "An error occured during migration");
+        }
+      }
+      host.Run();
     }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+.UseStartup<Startup>();
+  }
 }
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  };
+  componentDidMount() {
+    axios
+      .get('https://localhost:5001/api/values')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          values: response.data
+        });
+      })
+      .catch(error => console.log(error));
+  }
+  render() {
+    return (
+      <div className='app'>
+        <h1>Hellow</h1>
+        <ul>
+          {this.state.values.map((value: any) => (
+            <li key={value.id}>{value.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
